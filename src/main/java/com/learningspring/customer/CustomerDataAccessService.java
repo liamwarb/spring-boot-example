df8b56b38f5901c5d3dab8bf95058fdf9a717f0e
Liam Warburton <liam.warburton@autotrader.co.uk>
2023-01-22 18:55:35 +0000

    Initial commit

---
+++ src/main/java/com/learningspring/customer/CustomerDataAccessService.java
@@ -0,0 +1,35 @@
+package com.learningspring.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerDataAccessService implements CustomerDao {
+
+    private static final List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        Customer customer1 = new Customer(1, "Alex", "alex@gmail.com", 21);
+        customers.add(customer1);
+        Customer customer2 = new Customer(2, "Frank", "frank@gmail.com", 45);
+        customers.add(customer2);
+        Customer customer3 = new Customer(3, "Emily", "emily@outlook.com", 29);
+        customers.add(customer3);
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return customers.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+    }
+}
---
+++ src/main/java/com/learningspring/customer/CustomerService.java
@@ -0,0 +1,28 @@
+package com.learningspring.customer;
+
+import com.learningspring.exception.ResourceNotFound;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerDao.selectAllCustomers();
+    }
+
+    public Customer getCustomerById(Integer id) {
+        return customerDao.selectCustomerById(id)
+                .orElseThrow(() -> new ResourceNotFound(
+                        "Customer with ID [%s] was not found."
+                                .formatted(id)));
+    }
+
+}
